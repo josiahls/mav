7470360d3073aebe3c046bf35b35fd4611489339
josiah <jklaivins@gmail.com>
2026-03-13 17:55:52 +0000

    squash: Switch to use conda-forge ffmpeg

--- tools/sym_link_stdlib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -L third_party/modular ] && [ -d $PIXI_PROJECT_ROOT/../modular ]; then 
-    ln -s $PIXI_PROJECT_ROOT/../modular third_party/modular
-else 
-    echo 'Modular directory already exists.'
-fi
